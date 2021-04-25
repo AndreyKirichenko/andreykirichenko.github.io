04e3ece09659c5a042f3c71e339e6bda734597a4
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2021-04-25 17:23:36 +0300

    deploy demo

---
+++ demo/precache-manifest.f9cfbcef912800c32cade8e1e43589cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80a37d2135dece5ea7ce2c9ca37e08e",
+    "url": "./demo/index.html"
+  },
+  {
+    "revision": "b81826ae28e02355f83c",
+    "url": "./demo/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "b81826ae28e02355f83c",
+    "url": "./demo/static/js/2.69a4e85c.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./demo/static/js/2.69a4e85c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb7c142129f5e17a2f2",
+    "url": "./demo/static/js/main.d99f0571.chunk.js"
+  },
+  {
+    "revision": "fabd55e9208bf8f5534f",
+    "url": "./demo/static/js/runtime-main.785f45fe.js"
+  }
+]);
